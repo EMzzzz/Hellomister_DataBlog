5b3699e5d475eb45e361395cf02f1a63f9a92592
Kelly <kelly@hellomister.com.au>
2019-08-23 12:09:10 +1000

    update do to overwrite

--- graphExample1.R
+++ graphExample1.R
@@ -1,12 +1,9 @@
-install.packages("wesanderson")
+
 library(tidyverse)
 library(lubridate)
 library(ggraph)
 library(tidygraph)
 library(scales)
-library(grid)
-library(wesanderson)
-library(egg)
 
 
 
@@ -63,59 +60,4 @@ cook6 <- ggraph(cook_tidy, layout = 'linear') +
         geom_edge_arc(aes(width=weight, colour=Monument_Type), alpha=.5) +
         theme_bw()+
         scale_x_continuous(limits=c(1770, 2020), breaks = c(1770, 1870, 1970, 2000)) 
-
-cook_theme <-  theme(axis.line=element_blank(),
-                     # axis.text.x=element_blank(),
-                     axis.text.y=element_blank(),
-                     axis.ticks=element_blank(),
-                     axis.title.x=element_blank(),
-                     axis.title.y=element_blank(),
-                     panel.background=element_blank(),
-                     panel.border=element_blank(),
-                     panel.grid.major=element_blank(),
-                     panel.grid.minor=element_blank(),
-                     plot.background=element_blank(),
-                     plot.title = element_text(family="Helvetica Neue", size=20, face="plain"),
-                     plot.subtitle =  element_text(family="Helvetica Neue Light", size=12.9, face="plain"),
-                     legend.background = element_blank())
-
-# names(wes_palettes)
-
-mr3 <- wes_palettes$Moonrise3
-wes_palette("Moonrise3")
-
-
-dot1 <- ggplot(cookExample, aes(Dedication_Year, fill=Figure), colour="white") + 
-        geom_vline(xintercept = c(1770, 1870, 1970, 2000), colour="lightgray")+
-        geom_dotplot(binwidth = .1, stackgroups = TRUE, binpositions = "all", dotsize = 10, stackdir = "down", stroke=0)+
-        scale_x_continuous(limits=c(1770, 2020), breaks = c(1770, 1870, 1970, 2000)) +
-        cook_theme +
-        theme(legend.position = "none",
-              title = element_blank()) +
-        scale_fill_manual(values = mr3[c(1,3,5)])
-        
-        
-
-cook5 <- ggraph(cook_tidy, layout = 'linear') + 
-        geom_vline(xintercept = c(1770, 1870, 1970, 2000), colour="lightgray")+
-        geom_edge_arc(aes(width=weight,edge_colour=Figure), alpha=.5) +
-        scale_edge_colour_manual(values = mr3[c(1,3,5)], name = "Name")+
-        scale_edge_linetype_identity(scale_name="Number of Monuments")+
-        scale_x_continuous(limits=c(1770, 2020), breaks = c(1770, 1870, 1970, 2000), position = "top") +
-        cook_theme +
-        labs(title="Visualising Cook Memorials",
-             subtitle="Monuments dedicated to the voyage of the Endeavour from 1770 until today") 
-        
-        
-        
-
-ggarrange(cook5, dot1, heights = c(8, 4))
-
-
-
-
-
-        
-
-
 
